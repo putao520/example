d3fe52cdf6b9a2f6b5d29412dc9243f624c2c7aa
Administrator <yuyao1022@hotmail.com>
2020-11-17 19:44:36 +0800

    Initial commit

---
+++ src/main/java/main/main.java
@@ -0,0 +1,9 @@
+package main.java.main;
+
+import common.java.JGrapeSystem.GscBooter;
+
+public class main {
+    public static void main( String[] args ) {
+        GscBooter.start("Setting");
+    }
+}
